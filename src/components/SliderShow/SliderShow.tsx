a3d40ab367cfb028930d2c0829e079769cb97a93
thanhnt <thanhngo7112005@gmail.com>
2026-07-12 23:02:21 +0700

    thêm ảnh sliderShow

--- src/components/SliderShow/SliderShow.tsx
+++ src/components/SliderShow/SliderShow.tsx
@@ -3,13 +3,15 @@ import styles from "../SliderShow/SliderShow.module.scss";
 
 import barber1 from "../../assets/images/banner.png";
 import barber2 from "../../assets/images/banner2.jpg";
-import barber3 from "../../assets/images/banner3.webp";
+import barber3 from "../../assets/images/banner3.jpg";
+import barber4 from "../../assets/images/banner4.png";
 
 
 const images = [
   barber1,
   barber2,
   barber3,
+  barber4,
 
 ];
 
